bc1e14b46d3630be8472104bb65d97e7af8b9712
Bianca <pontesbianca54@gmail.com>
2026-04-12 19:44:20 -0300

    Ajustes-Abril

---
+++ script.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu-list");
+const menuMobile = document.querySelector(".menu-mobile");
+const menuBtn = document.querySelector(".menu-btn");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("ativo");
+  menuMobile.classList.toggle("bg-ativo"); 
+  btn.textContent = btn.textContent === "☰" ? "✕" : "☰";
+});
+
+
+const openBtn = document.getElementById("open-modal");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("close-modal");
+
+openBtn.addEventListener("click", () => {
+  modal.classList.add("ativo");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("ativo");
+});
+
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.classList.remove("ativo");
+  }
+});
