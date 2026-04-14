86898b27221188a943528f710509c48cd94c04dd
Bianca <pontesbianca54@gmail.com>
2026-04-13 22:14:07 -0300

    ajustes-finais

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const menu = document.getElementById("menu-list");
 const menuMobile = document.querySelector(".menu-mobile");
 
 btn.addEventListener("click", () => {
-	menu.classList.toggle("ativo");
-	menuMobile.classList.toggle("bg-ativo");
-	btn.textContent = btn.textContent === "☰" ? "✕" : "☰";
+  menu.classList.toggle("ativo");
+  menuMobile.classList.toggle("bg-ativo");
+  btn.textContent = btn.textContent === "☰" ? "✕" : "☰";
 });
 
 const openBtn = document.getElementById("open-modal");
@@ -13,15 +13,15 @@ const modal = document.getElementById("modal");
 const closeBtn = document.getElementById("close-modal");
 
 openBtn.addEventListener("click", () => {
-	modal.classList.add("ativo");
+  modal.classList.add("ativo");
 });
 
 closeBtn.addEventListener("click", () => {
-	modal.classList.remove("ativo");
+  modal.classList.remove("ativo");
 });
 
 modal.addEventListener("click", (e) => {
-	if (e.target === modal) {
-		modal.classList.remove("ativo");
-	}
+  if (e.target === modal) {
+    modal.classList.remove("ativo");
+  }
 });
