782e9b809c70307b6564cb7a3084efec1565e3f7
Bianca <pontesbianca54@gmail.com>
2026-04-14 22:29:03 -0300

    ajuste-js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const btn = document.getElementById("menu-btn");
-const menu = document.getElementById("menu-list");
-const menuMobile = document.querySelector(".menu-mobile");
+const btn = document.getElementById('menu-btn')
+const menu = document.getElementById('menu-list')
+const menuMobile = document.querySelector('.menu-mobile')
 
-btn.addEventListener("click", () => {
-  menu.classList.toggle("ativo");
-  menuMobile.classList.toggle("bg-ativo");
-  btn.textContent = btn.textContent === '☰' ? '✕' : '☰';
-});
+btn.addEventListener('click', () => {
+  menu.classList.toggle('ativo')
+  menuMobile.classList.toggle('bg-ativo')
+  btn.textContent = btn.textContent === '☰' ? '✕' : '☰'
+})
 
-const openBtn = document.getElementById("open-modal");
-const modal = document.getElementById("modal");
-const closeBtn = document.getElementById("close-modal");
+const openBtn = document.getElementById('open-modal')
+const modal = document.getElementById('modal')
+const closeBtn = document.getElementById('close-modal')
 
-openBtn.addEventListener("click", () => {
-  modal.classList.add("ativo");
-});
+openBtn.addEventListener('click', () => {
+  modal.classList.add('ativo')
+})
 
-closeBtn.addEventListener("click", () => {
-  modal.classList.remove("ativo");
-});
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('ativo')
+})
 
-modal.addEventListener("click", (e) => {
+modal.addEventListener('click', (e) => {
   if (e.target === modal) {
-    modal.classList.remove("ativo");
+    modal.classList.remove('ativo')
   }
-});
+})
